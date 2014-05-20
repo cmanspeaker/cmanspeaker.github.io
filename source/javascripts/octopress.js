9fb16f2de52faeca83e19d0b74e8aa37e2ff88d7
Cliff Manspeaker <cliff@drivenewmedia.com>
2014-05-20 14:51:57 -0400

    Comment out navigation JS

--- source/javascripts/octopress.js
+++ source/javascripts/octopress.js
@@ -1,18 +1,18 @@
-function getNav() {
-  var mobileNav = $('nav[role=navigation] fieldset[role=search]').after('<fieldset class="mobile-nav"></fieldset>').next().append('<select></select>');
-  mobileNav.children('select').append('<option value="">Navigate&hellip;</option>');
-  $('ul[role=main-navigation]').addClass('main-navigation');
-  $('ul.main-navigation a').each(function(i, link) {
-    mobileNav.children('select').append('<option value="'+link.href+'">&raquo; '+link.text+'</option>');
-  });
-  $('ul.subscription a').each(function(i, link) {
-    mobileNav.children('select').append('<option value="'+link.href+'">&raquo; '+link.text+'</option>');
-  });
-  mobileNav.children('select').bind('change', function(event) {
-    if (event.target.value) { window.location.href = event.target.value; }
-  });
-  mobileNav.children('select').val('');
-}
+// function getNav() {
+//   var mobileNav = $('nav[role=navigation] fieldset[role=search]').after('<fieldset class="mobile-nav"></fieldset>').next().append('<select></select>');
+//   mobileNav.children('select').append('<option value="">Navigate&hellip;</option>');
+//   $('ul[role=main-navigation]').addClass('main-navigation');
+//   $('ul.main-navigation a').each(function(i, link) {
+//     mobileNav.children('select').append('<option value="'+link.href+'">&raquo; '+link.text+'</option>');
+//   });
+//   $('ul.subscription a').each(function(i, link) {
+//     mobileNav.children('select').append('<option value="'+link.href+'">&raquo; '+link.text+'</option>');
+//   });
+//   mobileNav.children('select').bind('change', function(event) {
+//     if (event.target.value) { window.location.href = event.target.value; }
+//   });
+//   mobileNav.children('select').val('');
+// }
 
 function addSidebarToggler() {
   if(!$('body').hasClass('sidebar-footer')) {
